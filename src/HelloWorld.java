db16d5a09490b876b54c96541e8423ed97a24e37
feeneyp <feeneyp@patricks-mbp.lan>
2016-08-08 06:20:38 +0200

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		int i1 = 10;
+		int i2 = 3;
+		System.out.println("Hello World");
+		System.out.println((double) i1/ (double) i2);
+		
+	}
+
+}
